5e221e3acede3b21df05979b8b47290e575281f9
dipak <sahdeepak330@gmail.com>
2021-03-22 15:49:29 +0530

    Initial commit

---
+++ app/src/main/java/com/dpk/tiktaktoe/MainActivity.java
@@ -0,0 +1,134 @@
+package com.dpk.tiktaktoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button[][] buttons = new Button[3][3];
+    private boolean player1Turn = true;
+    private int roundCount;
+    private int player1Points;
+    private int player2Points;
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewPlayer1 = findViewById(R.id.text_view_p1);
+        textViewPlayer2 = findViewById(R.id.text_view_p2);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonId = "button_" + i + j;
+                int resId = getResources().getIdentifier(buttonId, "id", getPackageName());
+                buttons[i][j] = findViewById(resId);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             resetGame();
+            }
+        });
+    }
+
+    private void resetGame() {
+        player1Points=player2Points=0;
+        updatePointsText();
+        resetBoard();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+        if (player1Turn) {
+            ((Button) v).setText("X");
+        } else {
+            ((Button) v).setText("O");
+        }
+        roundCount++;
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn;
+        }
+    }
+
+    private void player2Wins() {
+        player2Points++;
+        FancyToast.makeText(this,"player 2 wins",Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        roundCount=0;
+        player1Turn=true;
+    }
+
+    private void updatePointsText() {
+        textViewPlayer1.setText("player 1: "+player1Points);
+        textViewPlayer2.setText("player 2: "+player2Points);
+    }
+
+    private void player1Wins() {
+        player1Points++;
+        FancyToast.makeText(MainActivity.this,"player 1 wins",Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void draw() {
+        FancyToast.makeText(this,"Draw",Toast.LENGTH_SHORT).show();
+            resetBoard();
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if ((field[i][0].equals(field[i][1])) && (field[i][0].equals(field[i][2])) && (!field[i][0].equals(""))) {
+                return true;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if ((field[0][i].equals(field[1][i])) && (field[0][i].equals(field[2][i])) && (!field[0][i].equals(""))) {
+                return true;
+            }
+        }
+        if ((field[0][0].equals(field[1][1])) && (field[0][0].equals(field[2][2])) && (!field[0][0].equals(""))) {
+            return true;
+        }
+        if ((field[0][2].equals(field[1][1])) && (field[0][2].equals(field[2][0])) && (!field[0][2].equals(""))) {
+            return true;
+        }
+        return false;
+    }
+}
